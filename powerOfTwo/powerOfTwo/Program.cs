499afd41305f4e6d25ea66667001538e8abd0aee
NoroMedafe <danya1515@inbox.ru>
2022-05-18 21:27:09 +0300

    start

---
+++ powerOfTwo/powerOfTwo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+
+            int minLimitRandomNumber = 2;
+            int maxLimitRandomNumber = 150;
+            int number = randomNumber.Next(minLimitRandomNumber, maxLimitRandomNumber);
+            int currentDegree = 1;
+            double secondDegree = 0.5;
+            double numberToThePower = 2;
+
+            for (int i = 0; number > numberToThePower; currentDegree++)
+            {
+                numberToThePower = numberToThePower / secondDegree;
+            }
+
+            Console.WriteLine(number);
+            Console.WriteLine(currentDegree);
+            Console.WriteLine(numberToThePower);
+
+            Console.ReadKey();
+        }
+    }
+}
